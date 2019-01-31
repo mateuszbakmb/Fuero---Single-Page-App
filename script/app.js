e6e5be8ee6fd2eab0d6646de6a7867c49d3c704a
mateuszbak.mb <mateuszbak.mb@gmail.com>
2019-01-31 21:11:58 +0100

    removed fetch at logging since it requires connecting with localhost, this way its possible to log in using any login/password combination

--- script/app.js
+++ script/app.js
@@ -11,40 +11,50 @@ if (document.querySelector('#usernameInput').value=='' || document.querySelector
 
 document.querySelector('#logIn').addEventListener("click", (e)=>{
   e.preventDefault();
-  const username = document.querySelector('#usernameInput').value;
-  const password = document.querySelector('#passwordInput').value;
+  // const username = document.querySelector('#usernameInput').value;
+  // const password = document.querySelector('#passwordInput').value;
+  //
+  // const url = 'http://localhost:5000/Login';
+  // let payload = {
+  //   "login" : username,
+  //   "password" : password
+  // };
+  //
+  // fetch(url, {
+  //   method: 'POST',
+  //   body: JSON.stringify(payload),
+  //   headers:{
+  //     'Content-Type': 'application/json'
+  //   }
+  // }).then(res => res.json())
+  // .then(response => {
+  //   console.log('Success:', response)
+  //   if(response===200){
+  //     document.querySelector('.invalid').classList.add('hidden');
+  //     document.querySelector('.loginBox').classList.remove('redShadow');
+  // //if I used a toggle in every example below, then switching from Configuration into Login screen
+  // //and logging back would put me back in configuration instead of Dashboard
+  //     document.querySelector('.dashboardhidden').classList.remove('d-none');
+  //     document.querySelector('.dashboardhidden').querySelector('h2').textContent = 'Dashboard';
+  //     document.querySelector('.window1').classList.remove('d-none');
+  //     document.querySelector('.window2').classList.add('d-none');
+  //     document.querySelector('.content').classList.toggle('d-none');
+  //   } else {
+  //     document.querySelector('.invalid').classList.remove('hidden');
+  //     document.querySelector('.loginBox').classList.add('redShadow');
+  //   }
+  // })
+  // .catch(error => console.error('Error:', error));
 
-  const url = 'http://localhost:5000/Login';
-  let payload = {
-    "login" : username,
-    "password" : password
-  };
-
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(payload),
-    headers:{
-      'Content-Type': 'application/json'
-    }
-  }).then(res => res.json())
-  .then(response => {
-    console.log('Success:', response)
-    if(response===200){
-      document.querySelector('.invalid').classList.add('hidden');
-      document.querySelector('.loginBox').classList.remove('redShadow');
-  //if I used a toggle in every example below, then switching from Configuration into Login screen
-  //and logging back would put me back in configuration instead of Dashboard
-      document.querySelector('.dashboardhidden').classList.remove('d-none');
-      document.querySelector('.dashboardhidden').querySelector('h2').textContent = 'Dashboard';
-      document.querySelector('.window1').classList.remove('d-none');
-      document.querySelector('.window2').classList.add('d-none');
-      document.querySelector('.content').classList.toggle('d-none');
-    } else {
-      document.querySelector('.invalid').classList.remove('hidden');
-      document.querySelector('.loginBox').classList.add('redShadow');
-    }
-  })
-  .catch(error => console.error('Error:', error));
+  document.querySelector('.invalid').classList.add('hidden');
+  document.querySelector('.loginBox').classList.remove('redShadow');
+//if I used a toggle in every example below, then switching from Configuration into Login screen
+//and logging back would put me back in configuration instead of Dashboard
+  document.querySelector('.dashboardhidden').classList.remove('d-none');
+  document.querySelector('.dashboardhidden').querySelector('h2').textContent = 'Dashboard';
+  document.querySelector('.window1').classList.remove('d-none');
+  document.querySelector('.window2').classList.add('d-none');
+  document.querySelector('.content').classList.toggle('d-none');
 });
 
 //logout
